137683e358cbb0dc14ab9c96d02cbbbdafd3511d
Matt Peachey <matthew_peachey@hotmail.com>
2019-03-17 17:52:31 -0300

    Added function requirements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,40 @@
+// Hello SASHY!  I have your next task for you!  Please update this function to change the border
+// color of the answers when you click submit.  Of course when you click next the borders should
+// all change back to black.  modify the function below!  I am proud of you! <3
+
 function submit(){
     console.log("click");
     if(document.getElementById('Submit').innerHTML=="Submit"){
+        //Make the 3 incorrect answers red border, and the correct one green.  Just assume that the
+        // correct one is the 3rd one for now.
+
+        // ----
+
+
+
+        // ----
         document.getElementById('Submit').innerHTML="Next →";
     }
     else{
+
+        //Make all of the borders black!
+        // Hint: use document.getElementByClassName();  -> this returns an array (gotta run a loop!)
+
+        // ----
+
+
+
+        // ----
         document.getElementById('Submit').innerHTML="Submit";
+
     }
+
+
+
+
+
+
+
 }
 
 function gryffindor(){
@@ -21,14 +50,14 @@ function ravenclaw(){
     document.getElementById('gameArea').style.backgroundColor="#946B2D";
     document.body.style.backgroundColor="#1259cc";
     document.getElementById('Submit').style.backgroundColor="#1259cc";
-    document.getElementById('Submit').style.color="black"; 
+    document.getElementById('Submit').style.color="black";
 }
 function slytherin(){
     console.log("click");
     document.getElementById('gameArea').style.backgroundColor="#AAAAAA";
     document.body.style.backgroundColor="#0D6217";
     document.getElementById('Submit').style.backgroundColor="#0D6217";
-    document.getElementById('Submit').style.color="#AAAAAA"; 
+    document.getElementById('Submit').style.color="#AAAAAA";
 }
 function hufflepuff(){
     console.log("click");
